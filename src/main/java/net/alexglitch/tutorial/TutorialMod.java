bde026c748d91de38569d4db730896d5a20e5bc7
ALEILGOD <aleilgod@gmail.com>
2025-10-06 20:18:25 +0200

    aggiunta oggetti 1

--- src/main/java/net/alexglitch/tutorial/TutorialMod.java
+++ src/main/java/net/alexglitch/tutorial/TutorialMod.java
@@ -1,5 +1,6 @@
 package net.alexglitch.tutorial;
 
+import net.alexglitch.tutorial.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -11,5 +12,6 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+        ModItems.registerModItems();
 	}
 }
